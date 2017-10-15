6634761ab8c9824ece3c4fa0ef8e35f47fb3e087
MonjeFunky <32797661+MonjeFunky@users.noreply.github.com>
2017-10-15 19:44:44 +0200

    Create Shuffle1.java

---
+++ Shuffle1.java
@@ -0,0 +1,13 @@
+public class shufflel {
+    public static void main (String [] args) {
+        int x = 3;
+    while (x > 0) {
+     if (x > 2) {
+         System.out.print("a");
+         x = x - 1;
+         System.out.print ("-");
+      }
+    }
+  }
+}   
+   
